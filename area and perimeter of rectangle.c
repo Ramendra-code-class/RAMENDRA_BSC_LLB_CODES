955b6ea0edce2e251ab3b5925fd54c5ee2fa0a91
Ramendra-code-class <shauryatripathimmm@gmail.com>
2025-08-28 12:30:08 +0530

    Add files via upload

---
+++ area and perimeter of rectangle.c
@@ -0,0 +1,13 @@
+// Program to find area and perimeter of rectangle.
+// Developed by Ramendra, Date:27/08/2025
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the length and breathe");
+    scanf("%d%d",&a,&b);
+    printf("the area is = %d\n", a*b);
+    printf("the perimeter is = %d\n", 2*(a+b));
+
+
+}
