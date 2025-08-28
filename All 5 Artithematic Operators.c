318a53f12b3bbdb200fa0243f837dae79fc88aa3
Ramendra-code-class <shauryatripathimmm@gmail.com>
2025-08-28 12:26:56 +0530

    Add files via upload

---
+++ All 5 Artithematic Operators.c
@@ -0,0 +1,15 @@
+// Program to apply all arithematic operators.
+// Developed by Ramendra, Date:27/08/2025
+#include <stdio.h>
+void main()
+{
+    int a,b;
+    printf("enter the numbers");
+    scanf("%d%d",&a,&b);
+    printf("the sum is = %d\n", a+b);
+    printf("the difference is = %d\n", a-b);
+    printf("the product is = %d\n", a*b);
+    printf("the modulous is = %d\n", a%b);
+    printf("the division is = %f\n", (a+0.0)/b);
+
+}
